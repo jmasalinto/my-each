8b36d4334f90f404fee210e3bf070d2649e2c122
Jamie Masalinto <jmasalinto@gmail.com>
2020-11-19 21:01:41 -0500

    commit tree f9f8487683c46569e377dd8fd765d9073070449d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+
+    collection
+  else
+    return "Hey! No block was given!"
+  end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  return i
 end
